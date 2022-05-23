ddfa95d74ce7fbfc5b9fb886be9d6db5dd5e4b3a
Hadi Ghadiri <ghadiri@arayehco.com>
2022-05-23 17:50:31 +0430

    init Commit

--- app/src/main/java/com/example/androidtest/MainActivity.java
+++ app/src/main/java/com/example/androidtest/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.androidtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.math.BigInteger;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +16,16 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
+        byte SN[] = new byte[]{(byte)0xBC,(byte)0xB8,(byte)0xB5,(byte)0x22};
+
+
+
+        int firstPart = SN[1];
+        byte temp[] = new byte[] { SN[2], SN[3]};
+        BigInteger secondPart = new BigInteger(temp);
+
+        Log.d("xeagle6913", "output : "+firstPart+secondPart.toString());
 
 
 
