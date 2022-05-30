194785f9ff467f5475206e25533c07453e7ac485
Hadi Ghadiri <ghadiri@arayehco.com>
2022-05-30 15:51:27 +0430

    6

--- app/src/main/java/com/example/androidtest/MainActivity.java
+++ app/src/main/java/com/example/androidtest/MainActivity.java
@@ -1,13 +1,38 @@
 package com.example.androidtest;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private final static String TAG = "xeagle6913";
+
+
+    @SuppressLint("MissingPermission")
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+
+        TelephonyManager telephonyManager = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
+
+        Log.d(TAG, "IMEI : "+ telephonyManager.getDeviceId());
+
+
+
+
+
     }
 }
